97b04a30000a7f428cdb700594a605f63879e244
Marcio Dala Corte Fernandes <marciodf@hotmail.com>
2019-08-16 08:59:03 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo "<h1>Openshift apresentação master Versao CRM versao 2.3</h1> ";
+echo "<h1>Openshift apresentação master Versao CRM versao 2.5</h1> ";
 echo $_SERVER['SERVER_ADDR'];
 ?>
