d67d9f132c6073117aa2bbaa5783b219bd43f627
Marcio Dala Corte Fernandes <marciodf@hotmail.com>
2019-09-13 10:59:07 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo "<h1>Openshift apresentação master resumida Versao MP 2.0 </h1> ";
+echo "<h1>Openshift apresentação master resumida Versao MPT Felipe 2.0 </h1> ";
 echo $_SERVER['SERVER_ADDR'];
 ?>
