7ce178804240451f1a84a8d0b1e50f902f3324b8
Marcio Dala Corte Fernandes <marciodf@hotmail.com>
2019-10-10 16:17:20 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <?php
-echo "<h1>Openshift apresentação master v0 </h1> ";
+echo "<h1>Openshift apresentação master v1 </h1> ";
 echo $_SERVER['SERVER_ADDR'];
 ?>
+https://master1.contoso.int:8443/apis/build.openshift.io/v1/namespaces/teste-10/buildconfigs/ppp/webhooks/260928dcdc6a3984/github
