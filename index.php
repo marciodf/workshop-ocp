90d1fe802b62486ed4ce26267d7025dfa0e50c73
Marcio Dala Corte Fernandes <marciodf@hotmail.com>
2020-02-21 10:02:21 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-echo "<h1>Openshift apresentação master - fabrica SP Versao 2.0</h1> ";
+echo "<h1>Openshift apresentação master - fabrica SP Versao 7.0 Sebrae</h1> ";
 echo $_SERVER['SERVER_ADDR'];
 ?>
